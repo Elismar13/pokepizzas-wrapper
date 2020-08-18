7359d4cdf12511bdd42360a278c8b246918823f4
Elismar13 <elismarsilva.5@outlook.com>
2020-08-18 13:49:05 -0300

    :tada: init

---
+++ src/main/java/com/thushima/pokepizza/PokepizzaApplication.java
@@ -0,0 +1,13 @@
+package com.thushima.pokepizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokepizzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokepizzaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thushima/pokepizza/PokepizzaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thushima.pokepizza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokepizzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
