f8cf215e78fb398d5797ac74f958c1fcd23a739e
Elismar13 <elismarsilva.5@outlook.com>
2020-08-18 14:02:57 -0300

    Product persistent class

---
+++ src/main/java/com/thushima/pokepizza/models/Product.java
@@ -0,0 +1,57 @@
+package com.thushima.pokepizza.models;
+
+public class Product {
+    private int id;
+    private String name;
+    private String description;
+    private String type;
+    private float price;
+
+    public Product(int id, String name, String description, String type, float price) {
+        setId(id);
+        setName(name);
+        setDescription(description);
+        setType(type);
+        setPrice(price);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+}
