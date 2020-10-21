86e94cf5cbb5424e561e0ac82bc531185b7efe60
Elismar13 <elismarsilva.5@outlook.com>
2020-10-21 13:56:37 -0300

    :ambulance: (chore) Inheritance of Pizza model fixed in Product

--- src/main/java/com/thushima/pokepizza/domain/models/Product.java
+++ src/main/java/com/thushima/pokepizza/domain/models/Product.java
@@ -1,13 +1,10 @@
 package com.thushima.pokepizza.domain.models;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
-@Entity
+@MappedSuperclass
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
